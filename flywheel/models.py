4cde1738d03956b3eaf3fa71f6f31ee47b3dd5d9
Steven Arcangeli <steven@highlig.ht>
2013-12-22 18:48:53 -0800

    Monkey patch boto to fix an issue with python 2.6

--- flywheel/models.py
+++ flywheel/models.py
@@ -1,15 +1,16 @@
 """ Model code """
 import time
 
-import contextlib
 import boto.dynamodb.types
+import contextlib
 import copy
 import inspect
-from boto.dynamodb2.fields import HashKey, RangeKey, AllIndex, GlobalAllIndex, BaseSchemaField
+from boto.dynamodb2.fields import (HashKey, RangeKey, AllIndex, GlobalAllIndex,
+                                   BaseSchemaField)
 from boto.dynamodb2.table import Table
 from boto.exception import JSONResponseError
 from collections import defaultdict
-from decimal import Inexact, Rounded
+from decimal import Inexact, Rounded, Decimal
 
 from .fields import Field, NUMBER
 
@@ -17,6 +18,16 @@ from .fields import Field, NUMBER
 # HACK to force conversion of floats to Decimals
 boto.dynamodb.types.DYNAMODB_CONTEXT.traps[Inexact] = False
 boto.dynamodb.types.DYNAMODB_CONTEXT.traps[Rounded] = False
+
+
+def float_to_decimal(f):
+    """ Monkey-patched replacement for boto's broken version """
+    n, d = f.as_integer_ratio()
+    numerator, denominator = Decimal(n), Decimal(d)
+    ctx = boto.dynamodb.types.DYNAMODB_CONTEXT
+    return ctx.divide(numerator, denominator)
+
+boto.dynamodb.types.float_to_decimal = float_to_decimal
 
 
 class ValidationError(Exception):
