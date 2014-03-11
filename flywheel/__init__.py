6e494b3e14e5adc07f1fd71e30b36b94fb37bbf6
Steven Arcangeli <stevearc@stevearc.com>
2014-03-11 14:44:39 -0700

    Bump version: 0.1.3 → 0.2.0rc1

--- flywheel/__init__.py
+++ flywheel/__init__.py
@@ -1,5 +1,5 @@
 """ flywheel """
-__version__ = '0.1.3'
+__version__ = '0.2.0rc1'
 
 from dynamo3 import (CheckFailed, ConditionalCheckFailedException, Binary,
                      STRING, NUMBER, BINARY, STRING_SET, NUMBER_SET,
