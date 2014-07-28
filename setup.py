e83c7af7eff48aeda71a36094c224a5609892900
Steven Arcangeli <stevearc@stevearc.com>
2014-07-28 09:26:59 -0700

    Tests run against python 3.4

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
             'Topic :: Database',
         ],
         author='Steven Arcangeli',
