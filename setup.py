6f29b7045489e5234c99468c992e53b06ef1f030
Steven Arcangeli <stevearc@stevearc.com>
2015-02-23 00:15:09 -0800

    Require up-to-date dynamo3 with bugfixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ CHANGES = open(os.path.join(HERE, 'CHANGES.rst')).read()
 CHANGES = re.sub(r'\(\s*:(issue|pr|sha):.*?\)', '', CHANGES)
 
 REQUIREMENTS = [
-    'dynamo3>=0.3.0',
+    'dynamo3>=0.3.2',
     'six'
 ]
 
