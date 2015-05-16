c6098745633b489e2dbdcbd856efe63a2734aa7f
Steven Arcangeli <stevearc@stevearc.com>
2015-05-15 23:19:06 -0700

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if sys.version_info[:2] < (2, 7):
 if __name__ == "__main__":
     setup(
         name='flywheel',
-        version='0.4.1',
+        version='0.4.2',
         description="SQLAlchemy-style ORM for Amazon's DynamoDB",
         long_description=README + '\n\n' + CHANGES,
         classifiers=[
