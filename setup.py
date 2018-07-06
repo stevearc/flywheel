bdfa563d094a238996a9f293afcc33dfbdfea904
Steven Arcangeli <stevearc@stevearc.com>
2018-07-05 22:02:18 -0700

    Attempt to fix travis build

--- setup.py
+++ setup.py
@@ -25,9 +25,6 @@ TEST_REQUIREMENTS = [
     'mock',
 ]
 
-if sys.version_info[:2] < (2, 7):
-    TEST_REQUIREMENTS.extend(['unittest2'])
-
 if __name__ == "__main__":
     setup(
         name='flywheel',
@@ -41,11 +38,11 @@ if __name__ == "__main__":
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Database',
         ],
         author='Steven Arcangeli',
